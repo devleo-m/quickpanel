b4da7ae239b2d31ddfcac53ef9704b3bd405417b
Leonardo <149954966+devleo-m@users.noreply.github.com>
2024-06-15 22:10:57 -0300

    banner completo

--- frontend/quickpanel-front/src/app/app.component.ts
+++ frontend/quickpanel-front/src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MenuComponent } from './menu/menu.component';
+import { BannerComponent } from './banner/banner.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
-    RouterOutlet, MenuComponent
+    RouterOutlet, MenuComponent, BannerComponent
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
