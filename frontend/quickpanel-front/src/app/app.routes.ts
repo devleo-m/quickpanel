df3b73eab634719ae465503b5f8e4d3b6b225e65
Leonardo <149954966+devleo-m@users.noreply.github.com>
2024-06-16 20:38:14 -0300

    Update all

--- frontend/quickpanel-front/src/app/app.routes.ts
+++ frontend/quickpanel-front/src/app/app.routes.ts
@@ -1,7 +1,14 @@
 import { Routes } from '@angular/router';
 import { BannerComponent } from "./components/index/banner/banner.component";
+import { LoginComponent } from "./components/login/login.component"; // Importe o LoginComponent
 
-export const routes: Routes = [{
-    path: '',
+export const routes: Routes = [
+  {
+    path: '', // Rota para a página inicial
     component: BannerComponent
-}];
+  },
+  {
+    path: 'login', // Rota para a página de login
+    component: LoginComponent
+  }
+];
