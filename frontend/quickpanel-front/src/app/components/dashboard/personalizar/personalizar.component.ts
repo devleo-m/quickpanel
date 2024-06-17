443bb73dc4493dc9708a1e20c206343dc8e8abe5
Leonardo <149954966+devleo-m@users.noreply.github.com>
2024-06-16 23:49:56 -0300

    componentes criado

---
+++ frontend/quickpanel-front/src/app/components/dashboard/personalizar/personalizar.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-personalizar',
+  standalone: true,
+  imports: [],
+  templateUrl: './personalizar.component.html',
+  styleUrl: './personalizar.component.scss'
+})
+export class PersonalizarComponent {
+
+}
