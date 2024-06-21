2c1f2cc8e9428087d0fa7a86b972bf123f5bd5ed
Leonardo <149954966+devleo-m@users.noreply.github.com>
2024-06-21 20:30:52 -0300

    roles and users

---
+++ backend/db/migrate/20240621214214_create_users.rb
@@ -0,0 +1,12 @@
+class CreateUsers < ActiveRecord::Migration[7.1]
+  def change
+    create_table :users do |t|
+      t.string :email, null: false
+      t.string :password_digest, null: false
+      t.references :role, null: false, foreign_key: true
+
+      t.timestamps
+    end
+    add_index :users, :email, unique: true
+  end
+end
