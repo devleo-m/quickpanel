b1940ad396e9cecaf0c1cbafc3bdba4482615edb
Leonardo <149954966+devleo-m@users.noreply.github.com>
2024-06-24 23:57:00 -0300

    Update user.rb

--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -1,4 +1,13 @@
 class User < ApplicationRecord
   belongs_to :role
   has_secure_password
+
+  validates :email, presence: true, uniqueness: true
+  validates :password, presence: true, length: { minimum: 6 }, if: :password_required?
+
+  private
+
+  def password_required?
+    new_record? || !password.nil?
+  end
 end
