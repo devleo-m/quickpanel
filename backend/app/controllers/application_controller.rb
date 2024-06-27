89f5f20a067facc5e14187ffce8df54ee22e15fb
Leonardo <149954966+devleo-m@users.noreply.github.com>
2024-06-27 18:43:37 -0300

    Update application_controller.rb

--- backend/app/controllers/application_controller.rb
+++ backend/app/controllers/application_controller.rb
@@ -1,14 +1,22 @@
 class ApplicationController < ActionController::Base
-  def authenticate_request
+  before_action :authorize_request
+
+  private
+
+  def authorize_request
     header = request.headers['Authorization']
     header = header.split(' ').last if header
     begin
-      decoded = JWT.decode(header, Rails.application.secret_key_base, true, { algorithm: 'HS256' })
+      decoded = JWT.decode(header, Rails.application.secret_key_base, true, algorithm: 'HS256')
       @current_user = User.find(decoded[0]['user_id'])
     rescue ActiveRecord::RecordNotFound => e
       render json: { errors: e.message }, status: :unauthorized
     rescue JWT::DecodeError => e
       render json: { errors: e.message }, status: :unauthorized
     end
+  end
+
+  def current_user
+    @current_user
   end
 end
