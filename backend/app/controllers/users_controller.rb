e7b6e3c24127c88ac0d3f548c0ede916549a5774
Leonardo <149954966+devleo-m@users.noreply.github.com>
2024-06-27 16:12:13 -0300

    Update users_controller.rb

--- backend/app/controllers/users_controller.rb
+++ backend/app/controllers/users_controller.rb
@@ -1,14 +1,16 @@
 class UsersController < ApplicationController
+  skip_before_action :verify_authenticity_token
   before_action :require_login, only: [:update, :me]
 
   def create
     @user = User.new(user_params)
-    @user.role = Role.find_by(name: 'leitor') # Usuário padrão é leitor
+    @user.role = Role.find_by(name: 'leitor') # Definindo o papel como leitor por padrão
 
     if @user.save
       session[:user_id] = @user.id
       render json: { message: 'Usuário criado com sucesso', user: @user }, status: :created
     else
+      Rails.logger.error("Erro ao criar usuário: #{@user.errors}")
       render json: { errors: @user.errors.full_messages }, status: :unprocessable_entity
     end
   end
@@ -19,6 +21,7 @@ class UsersController < ApplicationController
     if @user.update(user_params)
       render json: { message: 'Dados atualizados com sucesso', user: @user }, status: :ok
     else
+      Rails.logger.error("Erro ao atualizar usuário: #{@user.errors}")
       render json: { errors: @user.errors.full_messages }, status: :unprocessable_entity
     end
   end
@@ -30,13 +33,13 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:email, :password, :password_confirmation, :name, :address, :phone)
+    params.require(:user).permit(:email, :password, :password_confirmation)
   end
 
   def require_login
-      render json: {
-        error: 'Você deve estar logado para acessar esta seção' },
-             status: :unauthorized unless logged_in?
+    unless logged_in?
+      render json: { error: 'Você deve estar logado para acessar esta seção' }, status: :unauthorized
+    end
   end
 
   def logged_in?
