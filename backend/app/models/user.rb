a7b2c2a9e130500928add6ebf88be33899cf5728
Leonardo <149954966+devleo-m@users.noreply.github.com>
2024-06-27 16:12:09 -0300

    Update user.rb

--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -1,13 +1,15 @@
 class User < ApplicationRecord
-  belongs_to :role
-  has_secure_password
+  belongs_to :role  # Define a associação com a tabela de papéis (roles)
+  has_secure_password  # Garante que a senha seja criptografada antes de ser armazenada no banco de dados
 
-  validates :email, presence: true, uniqueness: true
+  validates :email, presence: true, uniqueness: true  # Valida que o email seja único e não esteja em branco
   validates :password, presence: true, length: { minimum: 6 }, if: :password_required?
+  # Valida que a senha esteja presente, tenha pelo menos 6 caracteres de comprimento, apenas se :password_required? for verdadeiro
 
   private
 
   def password_required?
     new_record? || !password.nil?
+    # Retorna verdadeiro se o registro for novo (ou seja, ainda não foi salvo no banco de dados) ou se a senha não estiver em branco
   end
 end
